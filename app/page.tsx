4097e4e49e914427646b441587479a6337797f09
JonathanWbn <jwieben@hey.com>
2023-07-09 09:22:31 +0200

    feat: trash all members

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Page() {
               onChange={handleChange}
               onDelete={handleDelete}
             />
-            {!picked && <TeamCount team={team} />}
+            {!picked && <TeamCount team={team} onDelete={() => setTeam([])} />}
           </div>
         )}
       </div>
--- components/team-count.tsx
+++ components/team-count.tsx
@@ -1,13 +1,23 @@
 import { motion } from "framer-motion";
 import { Team } from "../app/page";
+import { TrashIcon } from "./trash-icon";
 
-export const TeamCount = ({ team }: { team: Team }) =>
+export const TeamCount = ({
+  team,
+  onDelete,
+}: {
+  team: Team;
+  onDelete: VoidFunction;
+}) =>
   team.length > 1 ? (
-    <motion.div
-      className="mr-2 mt-2 text-right text-sm tracking-wide text-gray-400"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-    >
-      {team.length} members
-    </motion.div>
+    <div className="group mr-1 mt-3 flex items-center justify-end gap-2">
+      <motion.div
+        className="text-sm tracking-wide text-gray-400"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        {team.length} members
+      </motion.div>
+      <TrashIcon onClick={onDelete} />
+    </div>
   ) : null;
